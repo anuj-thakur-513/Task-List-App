9677dfdcb98150de9da4686348a8656f0be74107
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-08-04 04:58:20 +0530

    created ToDoViewModel

---
+++ app/src/main/java/com/project/todolist/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,31 @@
+package com.project.todolist.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.project.todolist.data.ToDoDatabase
+import com.project.todolist.data.models.ToDoData
+import com.project.todolist.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application): AndroidViewModel(application) {
+    private val toDoDAO = ToDoDatabase.getDatabase(application).toDoDao()
+    private val repository: ToDoRepository
+
+    private val getAllData: LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(toDoDAO)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData){
+        // we launch a coroutine in order to add data in the background thread
+        viewModelScope.launch(Dispatchers.IO){
+            repository.insertData(toDoData)
+        }
+    }
+
+}
