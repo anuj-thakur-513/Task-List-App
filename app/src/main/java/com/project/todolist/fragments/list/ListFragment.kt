6dc16970a49f19b05aa2dc5473cbf5edfe7261e7
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-08-04 07:19:18 +0530

    display data in recycler view

--- app/src/main/java/com/project/todolist/fragments/list/ListFragment.kt
+++ app/src/main/java/com/project/todolist/fragments/list/ListFragment.kt
@@ -5,13 +5,22 @@ import android.view.*
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.todolist.R
+import com.project.todolist.data.viewmodel.ToDoViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 
 class ListFragment : Fragment() {
+
+    // access to view model
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    // list adapter for recycler view
+    private val adapter: ListAdapter by lazy { ListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +28,17 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        // setting the adapter and layout manager of recycler view
+        val recyclerView = view.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        // we use the view model to get data from database and then observe it and send the data
+        // received to the adapter to update the layout
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            adapter.setData(data)
+        })
 
         // onClick listener for the fab
         view.floatingActionButton.setOnClickListener {
